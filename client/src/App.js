5ec991c6d888c54a193c16b8167017ec4f5057e8
Lukasz Kaminski <lukasznoroff@gmail.com>
2023-03-21 20:43:29 +0100

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SharedLayout/>} >
+            <Route index element={<Home />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/styles/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #898989;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+`;
+
+export default GlobalStyle;
